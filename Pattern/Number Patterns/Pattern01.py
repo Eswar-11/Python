00ab77247574cb1111d5363ab27cf04d1f5fb5c0
Eswar <56418600+Eswar-11@users.noreply.github.com>
2022-01-21 18:19:21 +0530

    Update Pattern01.py

--- Pattern/Number Patterns/Pattern01.py
+++ Pattern/Number Patterns/Pattern01.py
@@ -1,1 +1,18 @@
+"""
+Output:
+number:5
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+"""
 
+#code:
+n=int(input("number:"))
+x=0
+for i in range(0,n):
+    x += 1
+    for j in range(0, i+1):
+        print(x, end=" ")
+    print()
