89a39139c9fc45570297a11f25de067592c43ff5
Eswar <56418600+Eswar-11@users.noreply.github.com>
2022-01-21 18:39:18 +0530

    Create Pattern3.py

---
+++ Pattern/Number Patterns/Pattern3.py
@@ -0,0 +1,20 @@
+"""
+Output:
+number:5
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+
+
+"""
+
+#code:
+n=int(input("number:"))
+def pattern(n):
+    for i in range(n,0,-1):
+        for j in range(1, i+1):
+            print(j, end=" ")
+        print()
+pattern(n)
