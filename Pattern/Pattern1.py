c2cf9cefe9e54557144e0d5493ec188f3098bfba
Eswar <56418600+Eswar-11@users.noreply.github.com>
2022-01-21 17:35:09 +0530

    Update Pattern1.py

--- Pattern/Pattern1.py
+++ Pattern/Pattern1.py
@@ -1,8 +1,20 @@
+"""
+ Output:
+ number:5
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
+"""
+
+#code:
 n=int(input("number:"))
 k=n-1
 for i in range(0,n):
     for j in range(0,k):
-        print(end="-")
+        print(end=" ")
     k=k-1
     for j in range(0,i+1):
         print("* ",end="")
