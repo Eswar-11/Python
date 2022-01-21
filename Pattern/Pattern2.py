0865704913304da846ab3c3e47f6c0abdc14aa8b
Eswar <56418600+Eswar-11@users.noreply.github.com>
2022-01-21 17:38:54 +0530

    Create Pattern2.py

---
+++ Pattern/Pattern2.py
@@ -0,0 +1,21 @@
+"""
+Output:
+number:5
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+
+"""
+
+n=int(input("number:"))
+k=n
+for i in range(0,n):
+    for j in range(0,k):
+        print("* ",end="")
+    print("\r")
+    k=k-1
+    if i<n-1:
+        for i in range(0,i+1):
+            print(end=" ")
