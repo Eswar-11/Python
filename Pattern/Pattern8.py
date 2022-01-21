b008332e15f9dfb9019dd7d962822713178ab463
Eswar <56418600+Eswar-11@users.noreply.github.com>
2022-01-21 17:51:37 +0530

    Create Pattern8.py

---
+++ Pattern/Pattern8.py
@@ -0,0 +1,20 @@
+"""
+Output:
+number:5
+* * * * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+"""
+
+#downward half-pyramid
+#code:
+n=int(input("number:"))
+k=2*n-2
+for i in range(n,-1,-1):
+    for j in range(0,i+1):
+        print("*", end=" ")
+    print("\r")
