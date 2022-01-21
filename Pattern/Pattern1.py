6d3e5b6a0fd242c3183f2e737365157170d7b07c
Eswar <56418600+Eswar-11@users.noreply.github.com>
2022-01-21 17:28:08 +0530

    Update and rename Pattern1.java to Pattern1.py

---
+++ Pattern/Pattern1.py
@@ -0,0 +1,9 @@
+n=int(input("number:"))
+k=n-1
+for i in range(0,n):
+    for j in range(0,k):
+        print(end="-")
+    k=k-1
+    for j in range(0,i+1):
+        print("* ",end="")
+    print("\r")
